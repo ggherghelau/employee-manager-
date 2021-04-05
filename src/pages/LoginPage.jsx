bbe14ca445db3475c6078bcf0bac7e831656a079
Gigel Gherghelau <ggherghelau@yahoo,com>
2021-04-05 00:49:49 -0600

    assignment 4

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from 'styled-components'
+
+import FormInput from './../components/forms/FormInput'
+import Button from './../components/buttons/Button'
+                  
+
+const LoginPageStyles = styled.aside ` 
+
+   max-width: 380px;
+   margin: 6rem auto 0;
+
+   h1{
+       font-size: 2.25rem;
+   }
+       
+`
+
+const LoginPage = (props) => {
+    return (
+         <LoginPageStyles>
+            <header><h1>Login Page</h1></header>
+            <FormInput type="text" label="email"/>
+            <FormInput type="text" label="password"/>
+            <Button label="login" uiStyles="login"/>
+         </LoginPageStyles>
+  
+
+      );
+}
+
+export default LoginPage;
+
+ 
+
+
+
+ 
+
+
+
+ 
